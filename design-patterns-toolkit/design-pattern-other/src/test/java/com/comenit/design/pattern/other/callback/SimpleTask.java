c18f34890c16c5eec39647e904796931841f4441
comeNie <comenie@163.com>
2016-05-05 23:21:57 +0800

    add design pattern callback.

---
+++ design-patterns-toolkit/design-pattern-other/src/test/java/com/comenit/design/pattern/other/callback/SimpleTask.java
@@ -0,0 +1,11 @@
+package com.comenit.design.pattern.other.callback;
+
+/**
+ * Created by comeNie on 16/5/5.
+ */
+public class SimpleTask extends Task{
+
+    public void execute() {
+        System.out.println("do execute.");
+    }
+}
